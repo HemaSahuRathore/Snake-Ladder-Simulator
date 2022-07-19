249af91514d4a713ed1acdcd566e88e0e3e95793
Hema <hemasahu019@gmail.com>
2022-07-19 17:08:14 +0530

    [Hema Added Welcome to Snake & Ladder Program Page]

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace D4_SnakeNLadderSimulator
+{   /// <summary>
+    /// Welcome Page
+    /// </summary>
+    public class Program
+    {   //An entrypoint of console app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Welcome to Snake & Ladder Simulator Program]"); //welcome msg
+            Console.ReadLine();
+        }
+    }
+}
