9959ce0bffeb4c2cca6d71c1e88609b3241a24af
Hema <hemasahu019@gmail.com>
2022-07-19 17:13:21 +0530

    [Hema Added UC2 Code to generate die roll number]

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
 ﻿namespace D4_SnakeNLadderSimulator
 { /// <summary>
-  /// UC1 : Snake and Ladder game played with single player at start position 0 
+  /// UC2 : The Player rolls the die to get a number between 1 to 6
   /// </summary>
     public class Program
     {
         //Entrypoint of console app
         static void Main(string[] args)
         {   //constant variable
-            const int START_POSITION = 0; //start position remains same for each player
+            const int START_POSITION = 0;
 
             Console.WriteLine("Welcome to the Snake and Ladder Simulator Program");
-            Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION); //displaying start position
-            Console.ReadLine();
+            Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION);
+
+            Random random = new Random(); //Creating Random class object to access Next function
+            int dieRollNum = random.Next(1, 7); //using next function to generate number between 1 & 6
+            Console.WriteLine("After Rolling die, Dice Number : " + dieRollNum);
+
         }
     }
 }
