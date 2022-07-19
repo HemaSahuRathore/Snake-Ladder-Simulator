bc12890333ae0e5e6586a3212b2e09292a02a324
Hema <hemasahu019@gmail.com>
2022-07-19 17:10:16 +0530

    [Hema Added UC1 Player at Start Position 0]

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿namespace D4_SnakeNLadderSimulator
-{   /// <summary>
-    /// Welcome Page
-    /// </summary>
+{ /// <summary>
+  /// UC1 : Snake and Ladder game played with single player at start position 0 
+  /// </summary>
     public class Program
-    {   //An entrypoint of console app
+    {
+        //Entrypoint of console app
         static void Main(string[] args)
-        {
-            Console.WriteLine("[Welcome to Snake & Ladder Simulator Program]"); //welcome msg
+        {   //constant variable
+            const int START_POSITION = 0; //start position remains same for each player
+
+            Console.WriteLine("Welcome to the Snake and Ladder Simulator Program");
+            Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION); //displaying start position
             Console.ReadLine();
         }
     }
