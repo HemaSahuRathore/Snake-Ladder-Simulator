e24192221c269bd7f039694a6d081b5f5ca2731c
Hema <hemasahu019@gmail.com>
2022-07-19 17:40:58 +0530

    [Hema Added UC5 Refactoring code to stay in prev. position until reaches exact 100]

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace D4_SnakeNLadderSimulator
 { /// <summary>
-  /// UC4 : Repeat till the Player reaches the winning position 100.
-  /// In case the player position moves below 0, then the player restarts from 0
+  /// UC5 : Ensure the player gets to exact winning position 100. 
+  /// In case the player position go above 100, player stays in the same previous position till the player gets the exact
   /// </summary>
     public class Program
     {
@@ -14,22 +14,20 @@
             const int START_POSITION = 0;
             const int WINNING_POSITION = 100;
 
-
             Console.WriteLine("Welcome to the Snake and Ladder Simulator Program");
-            Console.WriteLine("Mode : Single Player");
             Console.WriteLine("\nThe Position of your TOKEN in the board is START : " + START_POSITION);
 
             Random random = new Random(); //Creating Random class object to access Next function
 
             while (position != WINNING_POSITION)
             {
-                int dieRoll = random.Next(1, 7); //using next function to generate number between 1 & 6
-                Console.WriteLine("\nAfter Rolling die, Dice Number : " + dieRoll);
-                int option = random.Next(1, 4); //Random function to generate options between 1 & 3
+                int dieRollNum = random.Next(1, 7); //using next function to generate number between 1 & 6
+                Console.WriteLine("\nAfter Rolling die, Dice Number : " + dieRollNum);
 
-                //Creating object of ReturnPosition class to access ReturnCurrentPosition method
-                ReturnPosition returnPositionObj = new ReturnPosition();
-                position = returnPositionObj.ReturnCurrentPosition(option, dieRoll, position);
+                int option = random.Next(1, 4); //Random function to generate options between 1 & 3
+                
+                ReturnPosition returnPositionObj = new ReturnPosition(); //Creating object of ReturnPosition class to access ReturnCurrentPosition method
+                position = returnPositionObj.ReturnCurrentPosition(option, dieRollNum, position); //storing current position to run the loop
 
             }
 
--- ReturnPosition.cs
+++ ReturnPosition.cs
@@ -9,7 +9,7 @@ namespace D4_SnakeNLadderSimulator
     public class ReturnPosition
     {
         //Method to match option, move token accordingly and return current position
-        public int ReturnCurrentPosition(int option, int dieRoll, int position)
+        public int ReturnCurrentPosition(int option, int dieRollNum, int position)
         {
             //Selection statement to select option and change position accordingly
             switch (option)
@@ -21,12 +21,12 @@ namespace D4_SnakeNLadderSimulator
                     break;
                 case 2: //Ladder
                     Console.WriteLine("Option : Ladder");
-                    position += dieRoll;
+                    position += dieRollNum;
                     Console.WriteLine("Your Current Position : " + position);
                     break;
                 default: //Snake
                     Console.WriteLine("Option : Snake");
-                    position -= dieRoll;
+                    position -= dieRollNum;
                     if (position < 0) //if position becomes less than 0, player restarts from START i.e 0
                     { position = 0; }
                     Console.WriteLine("Your Current Position : " + position);
